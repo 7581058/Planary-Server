e44c4848ccef9d616501769beb78b969f9ba0745
daseul <7581058@gmail.com>
2024-06-20 20:58:39 +0900

    ✨ Feat: user controller 구현

---
+++ controllers/users/user-controller.js
@@ -0,0 +1,90 @@
+import {
+  createUser,
+  getUserByUserEmail,
+} from "../../services/users/user-service.js";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+
+dotenv.config();
+
+const saltRounds = 10;
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password, birth } = req.body;
+
+    if (!username || !email || !password || !birth) {
+      return res.status(400).json({
+        success: false,
+        message: "Username, email, password, and birth are required",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const userData = { ...req.body, password: hashedPassword };
+
+    createUser(userData, (err, results) => {
+      if (err) {
+        console.error("Database query error:", err);
+        return res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "User registered successfully",
+      });
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const results = await getUserByUserEmail(email);
+
+    if (!results) {
+      return res.json({
+        success: false,
+        message: "Invalid ID or Password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, results.password);
+
+    if (isMatch) {
+      const token = jwt.sign(
+        { userId: results.user_id },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "36h",
+        }
+      );
+
+      return res.status(200).json({
+        success: true,
+        message: "Login successfully",
+        token: token,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid ID or Password",
+      });
+    }
+  } catch (err) {
+    console.error("Error logging in:", err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
