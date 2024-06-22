b89f18f9a9104cba685214adb7ce0acbcf9c8f3d
daseul <7581058@gmail.com>
2024-06-22 23:34:39 +0900

    ✨ Feat: dashboard widget min, max size constant 추가

---
+++ constants/widgetsSize.js
@@ -0,0 +1,34 @@
+export const WIDGET_PROFILE_MINW = 2;
+export const WIDGET_PROFILE_MAXW = 4;
+export const WIDGET_PROFILE_MINH = 1;
+export const WIDGET_PROFILE_MAXH = 2;
+
+export const WIDGET_CLOCK_MINW = 1;
+export const WIDGET_CLOCK_MAXW = 2;
+export const WIDGET_CLOCK_MINH = 1;
+export const WIDGET_CLOCK_MAXH = 2;
+
+export const WIDGET_PLAYER_MINW = 1;
+export const WIDGET_PLAYER_MAXW = 3;
+export const WIDGET_PLAYER_MINH = 1;
+export const WIDGET_PLAYER_MAXH = 2;
+
+export const WIDGET_CALENDAR_MINW = 1;
+export const WIDGET_CALENDAR_MAXW = 4;
+export const WIDGET_CALENDAR_MINH = 1;
+export const WIDGET_CALENDAR_MAXH = 3;
+
+export const WIDGET_TODO_MINW = 2;
+export const WIDGET_TODO_MAXW = 3;
+export const WIDGET_TODO_MINH = 1;
+export const WIDGET_TODO_MAXH = 3;
+
+export const WIDGET_DDAY_MINW = 1;
+export const WIDGET_DDAY_MAXW = 2;
+export const WIDGET_DDAY_MINH = 1;
+export const WIDGET_DDAY_MAXH = 2;
+
+export const WIDGET_TIMER_MINW = 1;
+export const WIDGET_TIMER_MAXW = 2;
+export const WIDGET_TIMER_MINH = 1;
+export const WIDGET_TIMER_MAXH = 2;
