4045c3830520dc47085c9cca604a39e38eece243
daseul <7581058@gmail.com>
2024-07-07 22:42:07 +0900

    ✨ Feat: 대시보드 router 추가

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import usersRouter from "./users/user-router.js";
-
+import boardRouter from "./board/board-router.js";
 const router = express.Router();
 
 router.use("/users", usersRouter);
+router.use("/dashboard", boardRouter);
 
 export default router;
