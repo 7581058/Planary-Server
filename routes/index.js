eac87265727de91650c52cea0c7a08f63be758f0
daseul <7581058@gmail.com>
2024-07-07 23:05:43 +0900

    ✨ Feat: 디데이 라우터 등록

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import usersRouter from "./users/user-router.js";
 import boardRouter from "./board/board-router.js";
+import ddayRouter from "./widget/dday-router.js";
+
 const router = express.Router();
 
 router.use("/users", usersRouter);
 router.use("/dashboard", boardRouter);
-
+router.use("/dday", ddayRouter);
 export default router;
